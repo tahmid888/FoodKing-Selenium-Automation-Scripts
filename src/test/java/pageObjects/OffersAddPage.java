20efe9f14be5066b7dc4a0332f50f5cd7e488489
tahmid888 <tahmidulanon@gmail.com>
2025-02-14 22:31:38 +0600

    Offers-Added

--- src/test/java/pageObjects/OffersAddPage.java
+++ src/test/java/pageObjects/OffersAddPage.java
@@ -1,5 +1,109 @@
 package pageObjects;
 
-public class OffersAddPage {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class OffersAddPage extends BasePage {
+
+	public OffersAddPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(xpath = "(//span[normalize-space()='Offers'])[1]")
+	WebElement lnkOffers;
+	@FindBy(xpath = "//span[normalize-space()=\"Add Offer\"]")
+	WebElement addOfferBtn;
+	@FindBy(xpath = "//input[@id=\"name\"]")
+	WebElement txtName;
+	@FindBy(xpath = "//input[@id=\"amount\"]")
+	WebElement txtAmount;
+	@FindBy(xpath = "//input[@id=\"active\"]")
+	WebElement radioActiveBtn;
+
+	// Dates
+	// Only Click Start Date
+	@FindBy(xpath = "(//input[contains(@class, 'dp__input')])[1]")
+	WebElement selectStartDate;
+
+	// Only Click End Date
+	@FindBy(xpath = "(//input[contains(@class, 'dp__input')])[2]")
+	WebElement selectEndDate;
+
+	// Only Start Date Select
+	@FindBy(xpath = "//div[contains(@class, 'dp__cell') and text()='15']")
+	WebElement selectSDate;
+
+	// Only End Date Select
+	@FindBy(xpath = "//div[contains(@class, 'dp__cell') and text()='25']")
+	WebElement selectEDate;
+	
+	@FindBy(xpath = "//div[contains(normalize-space(), \"Offer-\")]")
+	WebElement getTxtName;
+
+	// *************************************************************************//
+
+	// Wait object for explicit waits
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	public void clickOffers() {
+		wait.until(ExpectedConditions.elementToBeClickable(lnkOffers)).click();
+		// lnkOffers.click();
+	}
+
+	public void addOffersBtn() {
+		wait.until(ExpectedConditions.elementToBeClickable(addOfferBtn)).click();
+	}
+
+	public void setName(String name) {
+
+		txtName.sendKeys(name);
+	}
+
+	public void setDiscountPercentage(String amount) {
+
+		txtAmount.sendKeys(amount);
+	}
+
+	// Dates
+	public void clickStartDate() {
+		selectStartDate.click();
+	}
+
+	public void clickSDate() {
+		selectSDate.click();
+	}
+
+	public void clickEndDate() {
+		selectEndDate.click();
+	}
+
+	public void clickEDate() {
+		selectEDate.click();
+	}
+
+	public void selectFileUpload() {
+
+		WebElement fileInput = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='file' and @id='image']")));
+
+		String imagePath = "C:\\Users\\tahmi\\Downloads\\selenium\\demopngtest.png";
+		fileInput.sendKeys(imagePath);
+
+	}
+
+	public void clickActiveBtn() {
+
+		radioActiveBtn.click();
+	}
+	
+	public String getNameTxt() {
+		return 	getTxtName.getText();
+
+		}
 }
--- src/test/java/testCases/TC018_OffersAddTest.java
+++ src/test/java/testCases/TC018_OffersAddTest.java
@@ -1,5 +1,65 @@
 package testCases;
 
-public class TC018_OffersAddTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.CouponsAddPage;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.OffersAddPage;
+import testBase.BaseClass;
+
+public class TC018_OffersAddTest extends BaseClass{
+	
+	@Test()
+	public void addOffers() throws InterruptedException {
+
+		
+		try {
+
+			// Home Page
+			HomePage homePage = new HomePage(driver);
+			homePage.clickLogin();
+
+			// Login Page
+			LoginPage loginPage = new LoginPage(driver);
+			loginPage.setEmail(p.getProperty("adminEmail"));
+			loginPage.setPassword(p.getProperty("adminPassword"));
+			loginPage.clickLogin();
+			homePage.clickAccount();
+			homePage.clickDashboard();
+
+
+			// Offers Page
+			OffersAddPage offersAddPage = new OffersAddPage(driver);
+			offersAddPage.clickOffers();
+			offersAddPage.addOffersBtn();
+			offersAddPage.setName(p.getProperty("offerName"));
+			offersAddPage.setDiscountPercentage(p.getProperty("offerDiscountPercentage"));
+			offersAddPage.clickStartDate();
+			offersAddPage.clickSDate();
+			offersAddPage.clickEndDate();
+			offersAddPage.clickEDate();
+			offersAddPage.selectFileUpload();
+			
+			// Coupons Add Page
+			CouponsAddPage couponsPage = new CouponsAddPage(driver);
+			couponsPage.clickSaveBtn();
+			
+			
+			// Assert
+			String expectedName = offersAddPage.getNameTxt();
+			Assert.assertTrue(expectedName.contains("Offer-2025"), "Offer name 'Offer-2025' is not found in the extracted text!");
+
+			
+			
+			
+		} catch (Exception e) {
+			// Logs the exception for debugging
+			e.printStackTrace(); 
+            Assert.fail("Test failed due to an exception: " + e.getMessage());
+
+		}
+	}
 
 }
