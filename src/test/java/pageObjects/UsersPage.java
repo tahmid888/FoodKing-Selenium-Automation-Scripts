94021b56447b5ab2844e59f718927a44eb400215
tahmid888 <tahmidulanon@gmail.com>
2025-02-17 23:48:46 +0600

    Administrators-Added

--- src/test/java/pageObjects/UsersPage.java
+++ src/test/java/pageObjects/UsersPage.java
@@ -1,5 +1,79 @@
 package pageObjects;
 
-public class UsersPage {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class UsersPage extends BasePage {
+	
+	public UsersPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	@FindBy(xpath="(//span[normalize-space()='Administrators'])[1]")
+	WebElement linkAdministrator;
+	@FindBy(xpath="//span[normalize-space()='Add Administrator']")
+	WebElement addAdministratorBtn;
+	@FindBy(xpath="//input[@id='name']")
+	WebElement txtName;
+	@FindBy(xpath="//input[@id='email']")
+	WebElement txtEmail;
+	@FindBy(xpath="//input[@id='phone']")
+	WebElement txtPhone;
+	@FindBy(xpath="//input[@id='active']")
+	WebElement radioActiveBtn;
+	@FindBy(xpath="//input[@id='inactive']")
+	WebElement radioInActiveBtn;
+	@FindBy(xpath="//input[@id='password']")
+	WebElement txtPassword;
+	@FindBy(xpath="//input[@id='password_confirmation']")
+	WebElement txtPasswordConfirmation;
+	@FindBy(xpath="//input[@id='current_branch']")
+	WebElement radioCurrentBranch;
+	@FindBy(xpath="//input[@id='all_branch']")
+	WebElement radioAllBranch;
+
+
+	// Wait object for explicit waits
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	
+	
+	
+	public void clickAdministrator() {
+		wait.until(ExpectedConditions.elementToBeClickable(linkAdministrator)).click();
+	}
+	public void clickaddAdministratorBtn() {
+		wait.until(ExpectedConditions.elementToBeClickable(addAdministratorBtn)).click();
+	}
+	public void setName(String name) {
+		txtName.sendKeys(name);
+	}
+	public void setEmail(String email) {
+		txtEmail.clear();
+		txtEmail.sendKeys(email);
+	}
+	public void setPhone(String phone) {
+		txtPhone.sendKeys(phone);
+	}
+	public void activeBtn() {
+		radioActiveBtn.click();
+	}
+	public void setPassword(String password) {
+		txtPassword.sendKeys(password);
+	}
+	public void setPasswordConfirmation(String confirmPassword) {
+		txtPasswordConfirmation.sendKeys(confirmPassword);
+	}
+	public void allBranchBtn() {
+		//wait.until(ExpectedConditions.elementToBeClickable(addAdministratorBtn)).click();
+		radioAllBranch.click();
+	}
+	
+	
+	
 }
--- src/test/java/testCases/TC021_AdministratorsAddTest.java
+++ src/test/java/testCases/TC021_AdministratorsAddTest.java
@@ -1,5 +1,65 @@
 package testCases;
 
-public class TC021_AdministratorsAddTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.CouponDeletePage;
+import pageObjects.CouponsAddPage;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.OffersAddPage;
+import pageObjects.OffersDeletePage;
+import pageObjects.UsersPage;
+import testBase.BaseClass;
+
+public class TC021_AdministratorsAddTest extends BaseClass {
+
+	@Test()
+	public void administratorsAdd() {
+
+		try {
+
+			// Home Page
+			HomePage homePage = new HomePage(driver);
+			homePage.clickLogin();
+
+			// Login Page
+			LoginPage loginPage = new LoginPage(driver);
+			loginPage.setEmail(p.getProperty("adminEmail"));
+			loginPage.setPassword(p.getProperty("adminPassword"));
+			loginPage.clickLogin();
+			homePage.clickAccount();
+			homePage.clickDashboard();
+
+			// UsersPage Page
+			UsersPage usersPage = new UsersPage(driver);
+			usersPage.clickAdministrator();
+			usersPage.clickaddAdministratorBtn();
+			usersPage.setName(p.getProperty("administratorsName"));
+			usersPage.setEmail(p.getProperty("administratorsEmail"));
+			usersPage.setPhone(p.getProperty("administratorsPhone"));
+			usersPage.activeBtn();
+			usersPage.setPassword(p.getProperty("administratorsPassword"));
+			usersPage.setPasswordConfirmation(p.getProperty("administratorsConfirmPassword"));
+			usersPage.allBranchBtn();
+
+			// Coupons Page
+			CouponsAddPage couponsPage = new CouponsAddPage(driver);
+			couponsPage.clickSaveBtn();
+			
+
+			// Assert
+			//String expectedName = offersDeletePage.getNameTxt();
+			// System.out.println("Extracted Text: " + expectedName);
+
+			// Assert.assertNotEquals(expectedName, "Item was deleted successfully");
+
+		} catch (Exception e) {
+			// Logs the exception for debugging
+			e.printStackTrace();
+			Assert.fail("Test failed due to an exception: " + e.getMessage());
+
+		}
+	}
 
 }
